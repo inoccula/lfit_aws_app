67f6ef9f59fc39769daff93f20f238d17f0031ac
Kenichi Inoue <inoccu+la@gmail.com>
2025-02-06 16:47:02 +0900

    ヘッダに変更

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello, ECS!');
+    res.send('<h1>Hello, ECS!</h1>');
 });
 
 app.listen(PORT, () => {
